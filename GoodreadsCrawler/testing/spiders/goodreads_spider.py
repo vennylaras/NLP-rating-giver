344b2e458466f6963c5125410b6ddfd6af52b96e
Venny Larasati <venny_la@yahoo.com>
2016-11-17 21:57:31 +0700

    Fix crawler output

--- GoodreadsCrawler/testing/spiders/goodreads_spider.py
+++ GoodreadsCrawler/testing/spiders/goodreads_spider.py
@@ -18,9 +18,9 @@ class GoodreadsSpider(scrapy.Spider):
 		}.get(x, 0)
 
 	def parse(self, response):
-		for review in response.css("div.friendReviews") :			
+		for review in response.css("div.friendReviews") :
+			texts = review.css("div.section div.review div.left div.reviewText span.readable span")
 			yield{
 				'rating' : self.rate(review.css("div.section div.review div.left div.reviewHeader span.staticStars span.staticStar::text").extract_first()),
-				'text' : review.css("div.section div.review div.left div.reviewText span.readable span::text").extract()
+				'text' : "".join(texts.xpath(".//text()").extract())
 			}
-        
