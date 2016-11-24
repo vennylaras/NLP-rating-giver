c168668b623ebfec0af0a843fc1548899e909256
13513008 <riduuan81@yahoo.com>
2016-11-24 19:19:31 +0700

    selesai TT

--- NLPRatingGiver/src/ratinggiver/SentenceClassifier.java
+++ NLPRatingGiver/src/ratinggiver/SentenceClassifier.java
@@ -8,8 +8,8 @@ package ratinggiver;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Scanner;
+import weka.classifiers.meta.FilteredClassifier;
 import weka.classifiers.trees.RandomTree;
-import weka.classifiers.trees.SimpleCart;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 import weka.core.converters.ArffSaver;
@@ -26,13 +26,14 @@ public class SentenceClassifier {
     
     public static void main(String args[]) throws Exception {
         Scanner scan = new Scanner(System.in);
-        String fileName = "E:\\NLP-rating-giver\\NLPRatingGiver\\harry.potter.test.filtered.arff";//scan.nextLine();
+        String fileName = "E:\\NLP-rating-giver\\NLPRatingGiver\\harry.potter.test.arff";//scan.nextLine();
         
         ConverterUtils.DataSource source = new ConverterUtils.DataSource(fileName);
         Instances data = source.getDataSet();
-        data.setClassIndex(0);
+        data.setClassIndex(data.numAttributes() - 1);
         
-        SimpleCart cls = (SimpleCart) weka.core.SerializationHelper.read("E:\\NLP-rating-giver\\NLPRatingGiver\\treecombine.model");
+        
+        FilteredClassifier cls = (FilteredClassifier) weka.core.SerializationHelper.read("E:\\NLP-rating-giver\\NLPRatingGiver\\test.model");
         
         for(int i = 0; i < data.numInstances(); i++){
             double value = cls.classifyInstance(data.instance(i));
@@ -42,7 +43,7 @@ public class SentenceClassifier {
         
         ArffSaver saver = new ArffSaver();
         saver.setInstances(data);
-        saver.setFile(new File("E:/NLP-rating-giver/NLPRatingGiver/harry.potter.test.filtered.classified.arff"));
+        saver.setFile(new File("E:/NLP-rating-giver/NLPRatingGiver/harry.potter.test.classified.arff"));
         //saver.setDestination(new File("./data/test.arff"));   // **not** necessary in 3.5.4 and later
         saver.writeBatch();
     }
