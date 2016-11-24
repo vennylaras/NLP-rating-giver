cf572660d61daf8f6b733f2f391c88660241cbd4
Venny Larasati <venny_la@yahoo.com>
2016-11-24 13:58:22 +0700

    Add CART evaluation

--- NLPRatingGiver/src/ratinggiver/RatingGiver.java
+++ NLPRatingGiver/src/ratinggiver/RatingGiver.java
@@ -9,7 +9,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import static ratinggiver.Preprocessor.posTagger;
+import weka.classifiers.Evaluation;
 import weka.classifiers.trees.SimpleCart;
 import weka.core.Attribute;
 import weka.core.Instances;
@@ -22,8 +24,11 @@ import weka.filters.unsupervised.attribute.StringToWordVector;
  */
 public class RatingGiver {
     public static void main(String[] args) throws IOException, Exception{
+        // JSONLoader loader = new JSONLoader("dataset/harry_potter_1.json");
+        // loader.getReview();
+        
         BufferedReader br = new BufferedReader(
-                         new FileReader("harry.potter.test.arff"));
+                         new FileReader("harry.potter.test.filtered.arff"));
 
         ArffLoader.ArffReader arff = new ArffLoader.ArffReader(br);
         Instances testset = arff.getData();
@@ -37,15 +42,20 @@ public class RatingGiver {
         ArrayList<Attribute> attr = new ArrayList<>();
         for (int i = 1; i < trainset.numAttributes(); i++){
             attr.add(trainset.attribute(i));
-            System.out.println(trainset.attribute(i));
+            // System.out.println(trainset.attribute(i));
         }
         
         // Cocokin arff test set sama attr
         
         SimpleCart tree = new SimpleCart();
+        tree.buildClassifier(trainset);
+        // System.out.println(tree.toString());
         
-        // JSONLoader loader = new JSONLoader("dataset/harry_potter_1.json");
-        // loader.getReview();
+        Evaluation eval = new Evaluation(testset);
+        eval.evaluateModel(tree, testset);
+        System.out.println(eval.toSummaryString("\n\n\n\nFull Training\n============\n", false));
+        eval.crossValidateModel(tree, testset, 10, new Random());
+        System.out.println(eval.toSummaryString("\n\n\n\n10-Fold Cross Validation\n============\n", false));
         
         // ### CONTOH CARA PAKE calculateSentiment ###
         // String text = "I want to see Harry become a better person and be able to prove Draco and his stupid friends wrong about what they think";
